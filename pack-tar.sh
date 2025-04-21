c97941d90618b92b3812a47447fa90dfb1a4d37c
glistermelon <glisterm3lon@gmail.com>
2025-04-20 21:30:07 -0500

    Update convenience script

--- pack-tar.sh
+++ pack-tar.sh
@@ -1,1 +1,5 @@
 tar --exclude='alsa-volume-limiter/.vscode' --exclude='alsa-volume-limiter/releases' -cf alsa-volume-limiter.tar alsa-volume-limiter
+mv alsa-volume-limiter.tar alsa-volume-limiter/releases/0.1.0/alsa-volume-limiter-0.1.0.tar
+cd alsa-volume-limiter/releases/0.1.0
+tar -xf alsa-volume-limiter-0.1.0.tar
+sha256sum alsa-volume-limiter-0.1.0.tar
